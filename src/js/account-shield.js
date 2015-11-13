a7883957a4383662fad700a737e9e6500e5eaf03
Adam Knight <adam@revolutionarysystems.co.uk>
2015-11-13 15:48:48 +0000

    Updated to latest api changes

--- src/js/account-shield.js
+++ src/js/account-shield.js
@@ -1,33 +1,33 @@
 AccountShieldClient = function(url, accountId, userId) {
 
-	url = url + "/lwc/" + accountId + "/" + userId;
+	url = url + "/lwc/" + accountId + "/";
 
 	this.getUser = function(callback){
-		get(url, callback);
+		get(url + "user?userId=" + userId , callback);
 	}
 
 	this.getSessions = function(callback) {
-		get(url + "/sessions", callback);
+		get(url + "/sessions?userId=" + userId, callback);
 	}
 
 	this.getSession = function(sessionId, callback){
-		get(url + "/sessions/" + sessionId, callback);
+		get(url + "/sessions/" + sessionId + "?userId=" + userId, callback);
 	}
 
 	this.getSessionHistory = function(sessionId, callback){
-		get(url + "/sessions/" + sessionId + "/history", callback);
+		get(url + "/sessions/" + sessionId + "/history?userId=" + userId, callback);
 	}
 
 	this.getDevices = function(callback){
-		get(url + "/devices", callback);
+		get(url + "/devices?userId=" + userId, callback);
 	}
 
 	this.getDevice = function(deviceId, callback){
-		get(url + "/devices/" + deviceId, callback);
+		get(url + "/devices/" + deviceId + "?userId=" + userId, callback);
 	}
 
 	this.getSessionsForDevice = function(deviceId, callback){
-		get(url + "/devices/" + deviceId + "/sessions", callback);	
+		get(url + "/devices/" + deviceId + "/sessions?userId=" + userId, callback);	
 	}
 
 	function get(url, callback) {
